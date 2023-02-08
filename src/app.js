405787e7a4693669d12f0cbee451dafaa3dae5df
sadrozzy <sadrozzycompany@gmail.com>
2023-02-08 18:53:42 +0300

    another one init...

---
+++ src/app.js
@@ -0,0 +1,29 @@
+//Styles
+import '@/pages/app/app.scss'
+import '@/pages/common.scss'
+import '@/components/header/header.scss'
+import '@/components/sidebar/sidebar.scss'
+import '@/components/subHeader/subHeader.scss'
+
+//Icons
+import '@/assets/logo.svg'
+import '@/assets/icons/extendedSettings.svg'
+import '@/assets/icons/moon.svg'
+import '@/assets/icons/profile.svg'
+
+import '@/assets/icons/common.svg'
+import '@/assets/icons/featured.svg'
+import '@/assets/icons/folder.svg'
+import '@/assets/icons/recent.svg'
+import '@/assets/icons/trashcan.svg'
+
+import '@/assets/icons/upload.svg'
+import '@/assets/icons/add.svg'
+import '@/assets/icons/sort.svg'
+import '@/assets/icons/chevron.svg'
+import '@/assets/icons/grid.svg'
+
+//21
+//Fonts
+// import '@/assets/fonts/OpenSans-SemiBold.ttf'
+// import '@/assets/fonts/OpenSans-Regular.ttf'
--- webpack.config.js
+++ webpack.config.js
@@ -1,145 +1,162 @@
-const path = require('path');
-const PugPlugin = require('pug-plugin');
+const path = require('path')
+const globule = require('globule')
+const fs = require('fs')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const cssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+//const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+//const CopyWebpackPlugin = require('copy-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
 
-module.exports = (env, argv) => {
-  const isProd = argv.mode === 'production';
+    if (isProd) {
+        config.minimizer = [
+            new cssMinimizerPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
 
-  return {
-    mode: isProd ? 'production' : 'development',
-    devtool: isProd ? 'source-map' : 'inline-source-map',
-    //devtool: 'source-map',
-    stats: 'minimal',
+    return config
+}
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+const cssLoaders = extra => {
+    const loaders = [
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+    ]
 
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+const babelOptions = preset => {
+    const opts = {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins: [
+            '@babel/plugin-proposal-class-properties'
+        ]
+    }
+
+    if (preset) {
+        opts.presets.push(preset)
+    }
+
+    return opts
+}
+const jsLoaders = () => {
+    const loaders = [{
+        loader: 'babel-loader',
+        options: babelOptions()
+    }]
+
+    if (isDev) {
+        //loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+const plugins = () => {
+    // if (isProd) {
+    //     base.push(new BundleAnalyzerPlugin())
+    // }
+
+    return [
+        new HTMLWebpackPlugin({
+            template: path.join(__dirname, './src/pages/app/app.pug'),
+            filename: filename('html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ]
+}
+
+const components = globule
+    .find(["src/components/**/*.pug","!src/components/components.pug"])
+    .map((path) => path.split('/').pop())
+    .reduce((acc,currentItem) => acc + `include ${currentItem.replace('.pug', '')}/${currentItem}\n`, ``);
+
+fs.writeFile("src/components/components.pug", components, (err) => {
+    if (err) throw err
+    console.log("Components are generated automatically");
+});
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
     entry: {
-        index: 'src/views/auth/login.pug',
-        signup: 'src/views/auth/signup.pug',
-        forgot: 'src/views/auth/forgot.pug',
+        main: './app.js'
     },
-
     output: {
-      path: path.join(__dirname, 'dist'),
-      publicPath: 'auto',
-      // output filename of scripts
-      filename: 'assets/js/[name].[contenthash:8].js',
-      chunkFilename: 'assets/js/[name].[id].js',
-      clean: true,
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
-
     resolve: {
-      alias: {
-        Views: path.join(__dirname, 'src/views/'),
-        Images: path.join(__dirname, 'src/assets/images/'),
-        Fonts: path.join(__dirname, 'src/assets/fonts/'),
-        Styles: path.join(__dirname, 'src/styles/'),
-        Scripts: path.join(__dirname, 'src/scripts/'),
-      },
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
     },
-
-    plugins: [
-      // enable processing of Pug files from entry
-      new PugPlugin({
-        //verbose: !isProd, // output information about the process to console
-        pretty: !isProd, // formatting of HTML
-        extractCss: {
-          // output filename of styles
-          filename: 'assets/css/[name].[contenthash:8].css',
-        },
-      }),
-    ],
-
-    module: {
-      rules: [
-        {
-          test: /\.pug$/,
-          loader: PugPlugin.loader,
-          options: {
-            // enable filters only those used in pug
-            embedFilters: {
-              // :escape
-              escape: true,
-              // :code
-              code: {
-                className: 'language-',
-              },
-              // :highlight
-              highlight: {
-                verbose: !isProd,
-                use: 'prismjs', // name of a highlighting npm package, must be extra installed
-              },
-            },
-          },
-        },
-
-        // styles
-        {
-          test: /\.(css|sass|scss)$/,
-          use: ['css-loader', 'sass-loader'],
-        },
-
-        // fonts
-        {
-          test: /\.(woff2?|ttf|otf|eot|svg)$/,
-          type: 'asset/resource',
-          include: /assets[\\/]fonts/, // fonts from `assets/fonts` directory only, match posix and win paths
-          generator: {
-            // output filename of fonts
-            filename: 'assets/fonts/[name][ext][query]',
-          },
-        },
-
-        // images
-        {
-          test: /\.(png|svg|jpe?g|webp)$/i,
-          // resourceQuery: { not: [/inline/] }, // ignore images with `?inline` query
-          type: 'asset/resource',
-          // include: /assets[\\/]images/, // images from `assets/images` directory only, match posix and win paths
-          generator: {
-            // output filename of images
-            filename: 'assets/img/[name].[hash:8][ext]',
-          },
-        },
-
-        // inline images: png or svg icons with size < 4 KB
-        // {
-        //   test: /\.(png|svg)$/i,
-        //   type: 'asset',
-        //   include: /assets[\\/]images/,
-        //   exclude: /favicon/, // don't inline favicon
-        //   parser: {
-        //     dataUrlCondition: {
-        //       maxSize: 4 * 1024,
-        //     },
-        //   },
-        // },
-
-        // force inline svg file containing `?inline` query
-        // {
-        //   test: /\.(svg)$/i,
-        //   resourceQuery: /inline/,
-        //   type: 'asset/inline',
-        // },
-      ],
-    },
-
-    performance: {
-      hints: isProd ? 'error' : 'warning',
-      // in development mode the size of assets is bigger than in production
-      maxEntrypointSize: isProd ? 1024000 : 4096000,
-      maxAssetSize: isProd ? 1024000 : 4096000,
-    },
-
+    optimization: optimization(),
     devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      compress: true,
-      open: true, // open in default browser
-      watchFiles: {
-        paths: ['src/**/*.*'],
-        options: {
-          usePolling: true,
-        },
-      },
+        port: 4200,
+        hot: isDev
     },
-  };
-};
+    //devtool: isDev ? 'source-map' : '',
+    plugins: plugins(),
+    module: {
+        rules: [
+            // {
+            //     test: /\.html$/,
+            //     loader: "html-loader"
+            // },
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/icons/[name].[hash:8][ext]',
+                },
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: `assets/fonts/[name].[hash:8][ext]`,
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            }
+        ]
+    }
+}
