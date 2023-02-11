b7ba35d5a344d7642acc4f4cd6f0e938584db63c
Alexey <abelenkov2006@gmail.com>
2023-02-11 21:10:22 +0300

    Мой перFirst Pull Request like a first Ðdate...

---
+++ src/app.js
@@ -0,0 +1,22 @@
+//Styles
+import '@/pages/app/app.scss'
+import '@/pages/common.scss'
+import '@/components/components.scss'
+
+//Icons 2
+import '@/assets/logo.svg'
+import '@/assets/icons/extendedSettings.svg'
+import '@/assets/icons/moon.svg'
+import '@/assets/icons/profile.svg'
+
+import '@/assets/icons/common.svg'
+import '@/assets/icons/featured.svg'
+import '@/assets/icons/folder.svg'
+import '@/assets/icons/recent.svg'
+import '@/assets/icons/trashcan.svg'
+
+import '@/assets/icons/upload.svg'
+import '@/assets/icons/add.svg'
+import '@/assets/icons/sort.svg'
+import '@/assets/icons/chevron.svg'
+import '@/assets/icons/grid.svg'
---
+++ webpack.config.js
@@ -0,0 +1,170 @@
+const path = require('path')
+const globule = require('globule')
+const fs = require('fs')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const cssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new cssMinimizerPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
+const filename = ext => isDev ? `[name]${ext}` : `[contenthash]${ext}`
+const htmlFilename = name => isDev ? `${name}.html` : `[contenthash].html`
+const cssLoaders = extra => {
+    const loaders = [
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+    ]
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+const babelOptions = preset => {
+    const opts = {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins: [
+            '@babel/plugin-proposal-class-properties'
+        ]
+    }
+
+    if (preset) {
+        opts.presets.push(preset)
+    }
+
+    return opts
+}
+const jsLoaders = () => {
+    const loaders = [{
+        loader: 'babel-loader',
+        options: babelOptions()
+    }]
+
+    if (isDev) {
+        //loaders.push('eslint-loader')
+    }
+
+    return loaders
+}
+const plugins = () => {
+    return [
+        new HTMLWebpackPlugin({
+            template: path.join(__dirname, './src/pages/app/app.pug'),
+            filename: htmlFilename('app'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('.css')
+        })
+    ]
+}
+
+
+//Generation of Declaration
+const components_pug = globule
+    .find(["src/components/**/*.pug","!src/components/components.pug"])
+    .map((path) => path.split('/').pop())
+    .reduce((acc,currentItem) => acc + `include ${currentItem.replace('.pug', '')}/${currentItem}\n`, ``);
+
+const components_scss = globule
+    .find(["src/components/**/*.scss","!src/components/components.scss", "!src/components/variable.scss"])
+    .map((path) => path.split('/').pop())
+    .reduce((acc,currentItem) => acc + `@import "${currentItem.replace('.scss', '')}/${currentItem}";\n`, ``);
+
+fs.writeFile("src/components/components.pug", components_pug, (err) => {
+    if (err) throw err
+    console.log("PUG declaration generated successfully");
+});
+
+fs.writeFile("src/components/components.scss", components_scss, (err) => {
+    if (err) throw err
+    console.log("SCSS declaration generated successfully");
+});
+//Generation of Declaration
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './app.js',
+    },
+    output: {
+        filename: filename('.js'),
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: isDev
+                }
+            },
+            {
+                test: /\.less$/,
+                use: cssLoaders('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: `assets/images/${filename('[ext]')}`,
+                },
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: `assets/fonts/${filename('[ext]')}`,
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            }
+        ]
+    }
+}
